c2507ddaf8d033bf38bcace6b0d1a2d7bd014a3d
Marius <marius.budreika09@gmail.com>
2022-09-27 15:50:52 +0300

    added context to project

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,21 +1,17 @@
-import React, { ReactNode } from "react";
-import Todo from "../components/models/todo";
+import React, { useContext } from "react";
 import TodoItem from "./TodoItem";
 import styles from "./Todos.module.css";
+import { TodosContext } from "../store/todos-context";
 
-interface TodosProps {
-  items: Todo[];
-  onRemoveTodo: (id: number) => void;
-}
-
-const Todos: React.FC<TodosProps> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   return (
     <ul className={styles.todos}>
-      {props.items.map((item) => (
+      {todosCtx.items.map((item) => (
         <TodoItem
           key={item.id}
           text={item.text}
-          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
         />
       ))}
     </ul>
